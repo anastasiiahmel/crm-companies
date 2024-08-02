0751617f9049c4e46d11bb9f1a7ae2bf1fff0298
Nastya Khmelenko <126502776+anastasiiahmel@users.noreply.github.com>
2024-08-02 16:25:54 +0200

    added a page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import AddCompanyButton from './components/add-company-button';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'CRM',
+  description: 'Welcome to CRM-Companies',
+};
 
 export default function Home() {
   return (
-    <main>
+    <>
       <h1 className="text-xl">Home page</h1>
       <AddCompanyButton />
-    </main>
+    </>
   );
 }
